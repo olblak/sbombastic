91e15cf93d3455613f27d01f9c5b95310cdcc856
Fabrizio Sestito <fabrizio.sestito@suse.com>
2025-10-21 07:36:20 +0200

    refactor(worker): refactor storage cmd and add loglevel flag
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -5,42 +5,58 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
+	"flag"
 	"fmt"
 	"log/slog"
 	"os"
 
 	genericapiserver "k8s.io/apiserver/pkg/server"
-	"k8s.io/component-base/cli"
+	"k8s.io/klog/v2"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-
-	"github.com/kubewarden/sbomscanner/cmd/storage/server"
+	"github.com/kubewarden/sbomscanner/internal/apiserver"
+	"github.com/kubewarden/sbomscanner/internal/cmdutil"
 	"github.com/kubewarden/sbomscanner/internal/storage"
 )
 
 func main() {
-	os.Exit(run())
-}
+	var logLevel string
 
-func run() int {
-	// TODO: add CLI flags
+	flag.StringVar(&logLevel, "log-level", slog.LevelInfo.String(), "Log level.")
+	flag.Parse()
+
+	slogLevel, err := cmdutil.ParseLogLevel(logLevel)
+	if err != nil {
+		//nolint:sloglint // Use the global logger since the logger is not yet initialized
+		slog.Error("error initializing the logger", "error", err)
+		os.Exit(1)
+	}
 	opts := slog.HandlerOptions{
-		Level: slog.LevelDebug,
+		Level: slogLevel,
 	}
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &opts)).With("component", "storage")
-	ctx := genericapiserver.SetupSignalContext()
+	logger.Info("Starting storage")
 
+	// Kubernetes components use klog for logging, so we need to redirect it to our slog logger.
+	klog.SetSlogLogger(logger)
+
+	ctx := genericapiserver.SetupSignalContext()
+	if err := run(ctx, logger); err != nil {
+		logger.Error("failed to run server", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, logger *slog.Logger) error {
 	dbURI, err := os.ReadFile("/pg/uri")
 	if err != nil {
-		logger.Error("failed to read database URI", "error", err)
-		return 1
+		return fmt.Errorf("reading database URI: %w", err)
 	}
 
 	config, err := pgxpool.ParseConfig(string(dbURI))
 	if err != nil {
-		logger.Error("failed to parse database URI", "error", err)
-		return 1
+		return fmt.Errorf("parsing database URI: %w", err)
 	}
 
 	// Use the BeforeConnect callback so that whenever a connection is created or reset,
@@ -50,14 +66,14 @@ func run() int {
 	config.BeforeConnect = func(_ context.Context, connConfig *pgx.ConnConfig) error {
 		connConfig.Fallbacks = nil // disable TLS fallback to force TLS connection
 
-		var serverCA []byte
-		serverCA, err = os.ReadFile("/pg/tls/server/ca.crt")
+		serverCA, err := os.ReadFile("/pg/tls/server/ca.crt")
 		if err != nil {
-			return fmt.Errorf("failed to read database server CA certificate: %w", err)
+			return fmt.Errorf("reading database server CA certificate: %w", err)
 		}
+
 		caCertPool := x509.NewCertPool()
 		if !caCertPool.AppendCertsFromPEM(serverCA) {
-			return errors.New("failed to append database server CA certificate to pool")
+			return errors.New("appending database server CA certificate to pool")
 		}
 
 		connConfig.TLSConfig = &tls.Config{
@@ -66,33 +82,34 @@ func run() int {
 			MinVersion:         tls.VersionTLS12,
 			InsecureSkipVerify: false,
 		}
-
 		return nil
 	}
 
 	db, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
-		logger.Error("failed to create connection pool", "error", err)
-		return 1
+		return fmt.Errorf("creating connection pool: %w", err)
 	}
 	defer db.Close()
 
 	// Run migrations
 	if _, err := db.Exec(ctx, storage.CreateImageTableSQL); err != nil {
-		logger.Error("failed to create image table", "error", err)
-		return 1
+		return fmt.Errorf("creating image table: %w", err)
 	}
 	if _, err := db.Exec(ctx, storage.CreateSBOMTableSQL); err != nil {
-		logger.Error("failed to create sbom table", "error", err)
-		return 1
+		return fmt.Errorf("creating sbom table: %w", err)
 	}
 	if _, err := db.Exec(ctx, storage.CreateVulnerabilityReportTableSQL); err != nil {
-		logger.Error("failed to create vulnerability report table", "error", err)
-		return 1
+		return fmt.Errorf("creating vulnerability report table: %w", err)
 	}
 
-	options := server.NewWardleServerOptions(db, logger)
-	cmd := server.NewCommandStartWardleServer(ctx, options)
+	srv, err := apiserver.NewStorageAPIServer(db, logger)
+	if err != nil {
+		return fmt.Errorf("creating storage server: %w", err)
+	}
 
-	return cli.Run(cmd)
+	if err := srv.Start(ctx); err != nil {
+		return fmt.Errorf("starting storage server: %w", err)
+	}
+
+	return nil
 }
